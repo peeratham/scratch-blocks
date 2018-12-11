d42dd69c82caec167fb7cbf8f770e04b13102451
Peeratham Techapalokul <tpeera4@vt.edu>
2018-12-11 15:21:16 -0500

    adjust block color

--- core/colours.js
+++ core/colours.js
@@ -26,61 +26,61 @@ Blockly.Colours = {
   // SVG colours: these must be specificed in #RRGGBB style
   // To add an opacity, this must be specified as a separate property (for SVG fill-opacity)
   "motion": {
-    "primary": "#4C97FF",
-    "secondary": "#4280D7",
-    "tertiary": "#3373CC"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "looks": {
-    "primary": "#9966FF",
-    "secondary": "#855CD6",
-    "tertiary": "#774DCB"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "sounds": {
-    "primary": "#CF63CF",
-    "secondary": "#C94FC9",
-    "tertiary": "#BD42BD"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "control": {
-    "primary": "#FFAB19",
-    "secondary": "#EC9C13",
-    "tertiary": "#CF8B17"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "event": {
-    "primary": "#FFBF00",
-    "secondary": "#E6AC00",
-    "tertiary": "#CC9900"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "sensing": {
-    "primary": "#5CB1D6",
-    "secondary": "#47A8D1",
-    "tertiary": "#2E8EB8"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "pen": {
-    "primary": "#0fBD8C",
-    "secondary": "#0DA57A",
-    "tertiary": "#0B8E69"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "operators": {
-    "primary": "#59C059",
-    "secondary": "#46B946",
-    "tertiary": "#389438"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "data": {
-    "primary": "#FF8C1A",
-    "secondary": "#FF8000",
-    "tertiary": "#DB6E00"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   // This is not a new category, but rather for differentiation
   // between lists and scalar variables.
   "data_lists": {
-    "primary": "#FF661A",
-    "secondary": "#FF5500",
-    "tertiary": "#E64D00"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "more": {
-    "primary": "#FF6680",
-    "secondary": "#FF4D6A",
-    "tertiary": "#FF3355"
+    "primary": "#636363",
+    "secondary": "#636363",
+    "tertiary": "#bdbdbd"
   },
   "text": "#575E75",
   "workspace": "#F9F9F9",
